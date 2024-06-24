9ad63871ae867cd6294d6a3ae7febe80a6e970e8
ALI <fsmaccounting@hotmail.com>
2024-06-24 21:51:01 +0100

    function create

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-const btn = document.querySelector('.btn')
+const firstPhrases = [
+  'Believe in yourself,',
+  'Every day is a new opportunity,',
+  'You are capable of amazing things,',
+  'Dream big and dare to fail,',
+  'Stay positive and happy,',
+];
 
-addEventListener('click',()=>{
-    console.log('button clcik')
-})
+const secondQuotes = [
+  'you have the power to create change.',
+  'your hard work will pay off.',
+  'your potential is limitless.',
+  'challenges are what make life interesting.',
+  'pursue your dreams with passion.',
+];
+
+const thirdStatements = [
+  'Keep pushing forward!',
+  'You can do it!',
+  'Never give up!',
+  'Stay focused and never settle.',
+  'Your journey is just beginning.',
+];
+const btn = document.querySelector('.btn');
+const showNumber = document.querySelector('.showNumber');
+
+
+const random = (array) => array[Math.floor(Math.random() * array.length)];
+
+const generateMessage=()=>{
+    const first = random(firstPhrases)
+    const second = random(secondQuotes)
+    const third = random(thirdStatements)
+    return`${first} ${second} ${third}`
+}
+
+btn.addEventListener('click', () => {
+  const randomPhrase = generateMessage();
+  console.log(randomPhrase);
+  showNumber.innerHTML = randomPhrase;
+});
